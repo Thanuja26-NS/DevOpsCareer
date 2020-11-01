283664fcae1f6d7497e7201312cc9e3e70f8f39f
vishnu <uvishnuvardhana.reddy@tcs.com>
2020-11-01 17:39:37 +0000

    this is first commit in git

---
+++ commands.sh
@@ -0,0 +1,10 @@
+string_var="Hi Team, My name is Mithun Reddy L, working in IBM, Manyatha Techpark "
+
+echo "The value of string_var value is: " ${string_var}
+
+echo "The length of the string is: " ${#string_var}
+
+echo "The sub string is:"  ${string_var:20:14}
+
+#Index from right end  of the string
+
